ff3dddaeada7bf49e037e54ae8032e74fc6c1189
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-04-11 05:59:34 +0530

    Update new.c

--- new.c
+++ new.c
@@ -6,14 +6,15 @@ int main (){
   for (int i=0;i<3;i++){
     int count=0;
     for (int j=0;j<4;j++){
-      if (arr[i][j]==1}
+      if (arr[i][j]==1)
           count++;
           }
       if (maxCount < count){
         maxCount = count;
          maxIdx = i;
       }
-          printf("\n);
+  }
+          printf("\n");
        printf("The answer is %d",maxIdx);
                  return 0 ;
                  }
